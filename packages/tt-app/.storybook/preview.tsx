bd98d106022a2f2de0030aa9e37ea65e8895197e
MOZGIII <mike-n@narod.ru>
2021-01-31 12:35:35 +0300

    Add base decorator and theming to the storybooks

---
+++ packages/tt-app/.storybook/preview.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import NoSsr from "@material-ui/core/NoSsr";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { StoryContext } from "@storybook/addons";
+
+import { themeIds, getTheme } from "../src/theme";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+};
+
+const availableThemes = themeIds();
+
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: availableThemes[0],
+    toolbar: {
+      icon: "circlehollow",
+      items: availableThemes,
+    },
+  },
+};
+
+const withBase = (Story: React.ElementType, context: StoryContext) => {
+  const theme = getTheme(context.globals.theme);
+  return (
+    <NoSsr>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Story {...context} />
+      </ThemeProvider>
+    </NoSsr>
+  );
+};
+
+export const decorators = [withBase];
