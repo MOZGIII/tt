c571563adaea27f88009673578b0059ae36c6291
MOZGIII <mike-n@narod.ru>
2021-01-31 12:44:32 +0300

    Set storybook itself to use dark theme

--- packages/tt-app/.storybook/preview.tsx
+++ packages/tt-app/.storybook/preview.tsx
@@ -3,11 +3,15 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import NoSsr from "@material-ui/core/NoSsr";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { StoryContext } from "@storybook/addons";
+import { themes as storybookThemes } from "@storybook/theming";
 
 import { themeIds, getTheme } from "../src/theme";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  docs: {
+    theme: storybookThemes.dark,
+  },
 };
 
 const availableThemes = themeIds();
