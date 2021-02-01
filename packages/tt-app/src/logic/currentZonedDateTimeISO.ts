14e687c5ec53ac566559285cc65bb10beb1a380c
MOZGIII <mike-n@narod.ru>
2021-02-01 12:31:10 +0300

    Implement mocking support at currentZonedDateTimeISO

--- packages/tt-app/src/logic/currentZonedDateTimeISO.ts
+++ packages/tt-app/src/logic/currentZonedDateTimeISO.ts
@@ -1,7 +1,22 @@
 import { Temporal } from "proposal-temporal";
 
-// Useful in the tests to allow mocking.
+// eslint-disable-next-line functional/no-let
+let override: Temporal.ZonedDateTime | null = null;
+
+// A mock-friendly way to obtain the current time.
 const currentZonedDateTimeISO = (): Temporal.ZonedDateTime =>
-  Temporal.now.zonedDateTimeISO();
+  override || Temporal.now.zonedDateTimeISO();
 
 export default currentZonedDateTimeISO;
+
+// Set the value to return instead of the real time.
+// Useful in the tests to allow mocking.
+export const setOverride = (
+  newOverride: Temporal.ZonedDateTime | null
+): void => {
+  override = newOverride;
+};
+
+// Utility aliases.
+export const freeze: (time: Temporal.ZonedDateTime) => void = setOverride;
+export const unfreeze: () => void = () => setOverride(null);
