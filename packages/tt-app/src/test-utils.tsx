b2e0b56fcde713cec93d851a13069547ef6fa270
MOZGIII <mike-n@narod.ru>
2021-02-01 12:51:00 +0300

    Set up global test utils in the app

---
+++ packages/tt-app/src/test-utils.tsx
@@ -0,0 +1,30 @@
+import "@testing-library/jest-dom/extend-expect";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import NoSsr from "@material-ui/core/NoSsr";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { render, RenderOptions, RenderResult } from "@testing-library/react";
+import React, { ReactElement } from "react";
+
+import { getTheme } from "./theme";
+
+const AllTheProviders: React.FC = ({ children }) => {
+  const theme = getTheme("dark");
+  return (
+    <NoSsr>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </NoSsr>
+  );
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "queries">
+): RenderResult => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from "@testing-library/react";
+
+export { customRender as render };
