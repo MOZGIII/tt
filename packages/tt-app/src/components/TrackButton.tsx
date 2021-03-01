11b5fb6230a3d538e3339b976ebc5bdd58c2e019
MOZGIII <mike-n@narod.ru>
2021-03-01 16:01:13 +0300

    Greener button

--- packages/tt-app/src/components/TrackButton.tsx
+++ packages/tt-app/src/components/TrackButton.tsx
@@ -16,7 +16,7 @@ const useStyles = makeStyles((theme) =>
       background: (props: Props) =>
         props.isTracking
           ? "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
-          : "linear-gradient(45deg, #6BFE8B 30%, #8EFF53 90%)",
+          : "linear-gradient(45deg, #96F321 30%, #CBF321 90%)",
     },
   })
 );
