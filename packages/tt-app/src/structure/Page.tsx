57ae53a1d2e5c70ff7752feef25f5e9307362734
MOZGIII <mike-n@narod.ru>
2021-03-01 23:38:09 +0300

    Add the display of the records to the page

--- packages/tt-app/src/structure/Page.tsx
+++ packages/tt-app/src/structure/Page.tsx
@@ -1,29 +1,38 @@
 import Paper from "@material-ui/core/Paper";
-import React from "react";
+import React, { useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import TrackPanel from "../components/TrackPanel";
+import TrackRecordsList from "../components/TrackRecordsList";
 import currentZonedDateTimeISO from "../logic/currentZonedDateTimeISO";
 import { Dispatch, RootState } from "../store";
 
 const Page: React.FC = () => {
   const tracker = useSelector((state: RootState) => state.tracker);
+  const records = useSelector((state: RootState) => state.records);
   const dispatch = useDispatch<Dispatch>();
 
+  const recordsList = useMemo(() => Object.values(records).reverse(), [
+    records,
+  ]);
+
   return (
-    <Paper square>
-      <TrackPanel
-        trackingSince={tracker.trackingSince}
-        taskName={tracker.taskName}
-        onTaskNameChange={(val) => dispatch.tracker.setTaskName(val)}
-        onTrackingStart={() =>
-          dispatch.tracker.start(currentZonedDateTimeISO())
-        }
-        onTrackingStop={() =>
-          dispatch.tracker.stopAndRecord(currentZonedDateTimeISO())
-        }
-      />
-    </Paper>
+    <>
+      <Paper square>
+        <TrackPanel
+          trackingSince={tracker.trackingSince}
+          taskName={tracker.taskName}
+          onTaskNameChange={(val) => dispatch.tracker.setTaskName(val)}
+          onTrackingStart={() =>
+            dispatch.tracker.start(currentZonedDateTimeISO())
+          }
+          onTrackingStop={() =>
+            dispatch.tracker.stopAndRecord(currentZonedDateTimeISO())
+          }
+        />
+      </Paper>
+      <TrackRecordsList records={recordsList} />
+    </>
   );
 };
 
