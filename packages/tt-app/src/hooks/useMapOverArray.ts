09ef2569967f30a6aeed65b4ad44ffe52a3cf91f
MOZGIII <mike-n@narod.ru>
2022-10-15 03:59:46 +0400

    Rewrtie useMapOverArray to fix TypeScript issues

--- packages/tt-app/src/hooks/useMapOverArray.ts
+++ packages/tt-app/src/hooks/useMapOverArray.ts
@@ -1,24 +1,23 @@
 import { DependencyList, useMemo } from "react";
 
-type ArrayItem<T extends readonly unknown[]> = T extends readonly (infer I)[]
-  ? I
-  : never;
+type MapFn<T extends Array<any>, Idx extends keyof T, R> = (
+  item: T[Idx],
+  index: Idx,
+  array: T
+) => R;
 
-const useMapOverArray = <T extends readonly unknown[], C>(
+function mapOverArray<T extends Array<any>, F extends MapFn<T, number, R>, R>(
+  array: T,
+  fn: F
+): Array<R> {
+  return array.map(fn as any);
+}
+
+const useMapOverArray = <T extends any[], C>(
   list: T,
-  factory: (item: ArrayItem<T>, index: number, array: T) => C,
+  fn: MapFn<T, number, C>,
   deps: DependencyList | undefined
 ): Array<C> =>
-  useMemo(
-    () =>
-      list.map(
-        (factory as unknown) as (
-          item: unknown,
-          index: number,
-          array: readonly unknown[]
-        ) => C
-      ),
-    [factory, list].concat(deps || [])
-  );
+  useMemo(() => mapOverArray(list, fn), [fn, list, ...(deps || [])]);
 
 export default useMapOverArray;
