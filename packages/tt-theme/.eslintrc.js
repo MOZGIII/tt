e9a1f92c86688136cbacdadd1457103e9516a53e
MOZGIII <mike-n@narod.ru>
2022-10-15 20:55:26 +0400

    Add proper deps and scripts

---
+++ packages/tt-theme/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: "tsconfig.json",
+  },
+  extends: ["tt-react"],
+  settings: {
+    linkComponents: [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      { name: "Link", linkAttribute: "to" },
+    ],
+  },
+  overrides: [
+    {
+      files: [".eslintrc.js"],
+      env: { node: true },
+    },
+    {
+      files: ["**/*.spec.[tj]sx?", "**/.test.[tj]sx?"],
+      rules: {
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+      },
+    },
+    {
+      files: ["**/*.stories.[tj]sx"],
+      rules: {
+        "functional/immutable-data": "off",
+      },
+    },
+  ],
+};
